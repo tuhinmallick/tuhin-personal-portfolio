004b9eb57d9c0bba8dfa9777da3f398da39ef8f4
tuhinmallick <tuhin.mallick@fau.de>
2022-02-21 23:53:00 +0100

    solve conflict

--- server.js
+++ server.js
@@ -7,9 +7,10 @@ var app = express();
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 // make express look in the public directory for assets (css/js/img)
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "build")));
+
 
 // This route serves the React app
-app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "public", "index.html")));
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "build", "index.html")));
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
