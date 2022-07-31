b4fba7550c3dee94155be0a64bf24a70c1843132
Tuhin Mallick <67542764+tuhinmallick@users.noreply.github.com>
2022-07-31 12:32:46 +0200

    Update Testimonial.jsx

--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -31,6 +31,26 @@ export default function SimpleSlider() {
 
   const TestimonilContent = [
     {
+      imageName: "Dr. Ralph Debusmann",
+      desc: `Tuhin is a standout data scientist and coder - he gets things done.
+
+I had the pleasure of working with him for seven months at Forecasty.AI - where he served as a data scientist and I as the CTO.
+
+Tuhin's ability to build not only AI models but also entire UIs around them, and to blend in the latest developments e.g. around online ML, event streaming, cloud deployment and NLP, was fascinating.
+
+He was a fountain of ideas.
+
+Tuhin would be an asset to any team remotely having to deal with AI.`,
+      reviewerName: "Dr. Ralph Debusmann",
+      designation: "Co-Founder and Chief Technical Officer at Forecasty.AI",
+    },
+    {
+      imageName: "Sumanto dutta chowdhury",
+      desc: ` From the moment you meet Tuhin, you know he is there to do a great job. He has had to deal with an enormous amount of stressful situations while juggling so many responsibilities that were thrown at him as he was growing within Tata Consultancy Services and yet, he was still there helping us whenever we were in tight spots with our deadlines. Tuhin exudes confidence because he gives a 110% of himself to achieve his goals, no matter how far out of reach they may seem. Team player, go-getter, not to forget his strong Java web development abilities, that's Tuhin in a nutshell.`,
+      reviewerName: "Dr. Pascal Ochs",
+      designation: "Data Scientist & Product Manager at Forecasty.AI",
+    },
+    {
       imageName: "Sumanto dutta chowdhury",
       desc: ` From the moment you meet Tuhin, you know he is there to do a great job. He has had to deal with an enormous amount of stressful situations while juggling so many responsibilities that were thrown at him as he was growing within Tata Consultancy Services and yet, he was still there helping us whenever we were in tight spots with our deadlines. Tuhin exudes confidence because he gives a 110% of himself to achieve his goals, no matter how far out of reach they may seem. Team player, go-getter, not to forget his strong Java web development abilities, that's Tuhin in a nutshell.`,
       reviewerName: "Sumanto Dutta Chowdhury",
